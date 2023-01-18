77f17c53a57e85c591ce0acf2e6c18a49cab2cf3
danilkor <danilkor4444@gmail.com>
2023-01-18 20:19:06 +0100

    .gitignore

--- scripts/hasher.php
+++ scripts/hasher.php
@@ -6,3 +6,7 @@
     function passHasher($pass){
         return md5("solenaya" . $pass . "sol");
     }
+
+    function loginHasher($input){
+        return md5(date("Y-m-d H:i:s") . $input . ("Privet chtoli?"));
+    }
