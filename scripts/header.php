b835fa3d8d514bbc6867f0514efbd9da5af46f60
danilkor <danilkor4444@gmail.com>
2023-01-18 12:54:53 +0100

    Header avatar fix

--- scripts/header.php
+++ scripts/header.php
@@ -25,20 +25,23 @@
             ');
         } else if(isset($_COOKIE["username"])) {
             echo('
-                <div class="col-md-3 text-end">
-                    <div class="dropdown text-end">
-                        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="../../img/avatars'.$_COOKIE['avatar'].'.png" alt="mdo" width="32" height="32" class="rounded-circle">
-                        </a>
-                        <ul class="dropdown-menu text-small" style="">
-                            <li><a class="dropdown-item" href="#">lorem</a></li>
-                            <li><a class="dropdown-item" href="#">ispum</a></li>
-                            <li><a class="dropdown-item" href="#">dorem</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                    </div>
+            <div class="col-md-3 text-end">
+            <div class="row">
+                <div class="col-md-8"></div>
+                <div class="dropdown text-end col-md-4">
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="../../img/avatars/'.$_COOKIE['avatar'].'.png" alt="YOU" width="44" height="44" class="rounded-circle">
+                    </a>
+                    <ul class="dropdown-menu text-small">
+                        <li><a class="dropdown-item" href="#">lorem</a></li>
+                        <li><a class="dropdown-item" href="#">ispum</a></li>
+                        <li><a class="dropdown-item" href="#">dorem</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
                 </div>
+            </div>
+        </div>
             ');
         }
     ?>
