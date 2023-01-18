ccbe473b5ae291acf45a67f7d762194b65f542a9
danilkor <95537076+danilkor@users.noreply.github.com>
2023-01-18 20:32:42 +0100

    DELETE HASH

--- scripts/hasher.php
+++ scripts/hasher.php
@@ -1,12 +1,1 @@
-<?php 
 
-// To improve security from hacking from GitHub
-
-
-    function passHasher($pass){
-        return md5("solenaya" . $pass . "sol");
-    }
-
-    function loginHasher($input){
-        return md5(date("Y-m-d H:i:s") . $input . ("Privet chtoli?"));
-    }
